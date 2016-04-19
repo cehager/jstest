d67386ea8ed8959ec168e37f9d9652191fe7fc38
Charlie Hager <charlie80088@gmail.com>
2016-04-18 18:05:44 -0600

    another test commit

--- app/main.js
+++ app/main.js
@@ -2,6 +2,6 @@
 var browser_1 = require('angular2/platform/browser');
 var character_component_1 = require('./character.component');
 browser_1.bootstrap(character_component_1.CharacterComponent)
-    .then(function (success) { return console.log("Bootstrap is a success"); })
+    .then(function (success) { return console.log("Bootstraping is a success"); })
     .catch(function (error) { return console.log(error); });
 //# sourceMappingURL=main.js.map
--- app/main.ts
+++ app/main.ts
@@ -2,6 +2,6 @@
   import { CharacterComponent } from './character.component';
 
   bootstrap(CharacterComponent)
-    .then(success => console.log(`Bootstrap is a success`))
+    .then(success => console.log(`Bootstraping is a success`))
     .catch(error => console.log(error));
 
